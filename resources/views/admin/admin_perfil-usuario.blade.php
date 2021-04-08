df30be3726a7c2ba381e305996a9e093828496f0
Chris <chris.e1297@gmail.com>
2021-04-08 18:37:04 -0500

    El administrador puede ver el perfil de un usuario

---
+++ resources/views/admin/admin_perfil-usuario.blade.php
@@ -0,0 +1,47 @@
+@extends('admin/admin_demo')
+@foreach ($users as $user)
+@section('title','| Perfil de '.$user->nombre)
+
+
+@section('content')
+
+    <div class="section page-section">
+        <form method="" action="" enctype="">
+            @csrf
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 centrar">
+                        @if($user->foto == null )
+                            <img src="{{asset('files/no-photo.png')}}" alt="foto de perfil" class="rounded-circle profile-image shadow">
+                        @else
+                            <img src="{{asset('profile-images/'.$user->foto)}}" alt="foto de perfil" class="rounded-circle profile-image shadow">
+                        @endif
+                        {{-- <img src="" alt="foto de perfil" class="rounded-circle profile-image shadow"> --}}
+                        <br>
+                        <br>
+                        
+                        <br>
+                        <h3>{{$user->nombre}} {{$user->apellidop}} {{$user->apellidom}}</h3>
+                        <h5><span class="dept-name">{{$user->deptoNombre}}</span></h5>
+                        
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card profile-info">
+                            <div class="card-body">
+                                <h5 class="card-title">Acerca de mi:</h5>
+                                <div class="dropdown-divider"></div>
+                                <p>Telefono: <span>{{$user->telefono}}</span></p>
+                                <p>Email: <span>{{$user->email}}</span></p>
+                                <p>Departamento: <span>{{$user->deptoNombre}}</span></p>
+                                <p>Rol: <span>{{$user->rolNombre}}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                        
+                </div>
+            </div>
+         </form>
+    </div>
+    
+@endsection
+@endforeach
